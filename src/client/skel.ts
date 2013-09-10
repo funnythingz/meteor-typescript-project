5a943859dbc1f6dd0e6697e11491e03fd4195fae
funnythingz <hiroki@hackleberry.me>
2013-09-11 00:41:16 +0900

    meteor typescript project

---
+++ src/client/skel.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../d.ts/lib.d.ts"/>
+/// <reference path="../../d.ts/meteor.d.ts"/>
+/// <reference path="../../d.ts/underscore.d.ts"/>
+
+Template['hello']['greeting'] = function () {
+    return "Typescript-compile of Meteor";
+};
