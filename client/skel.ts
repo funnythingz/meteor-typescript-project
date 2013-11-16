919e5a8c0f2fef474c62ddc0db713e688a4af335
funnythingz <hiroki@hackleberry.me>
2013-11-16 16:53:20 +0900

    added test unko

--- client/skel.ts
+++ client/skel.ts
@@ -1,10 +1,26 @@
 /// <reference path="../packages/typescript-libs/node.d.ts" />
 /// <reference path="../packages/typescript-libs/meteor.d.ts" />
 
+module HOGE {
+
+    export class Ahya {
+
+        constructor(private message: string) {}
+
+        say(): string {
+            return this.message;
+        }
+
+    }
+
+}
+
+
 Template['hello'].helpers({
 
     greeting: function() {
-        return "Typescript-compile of Meteor";
+        var ahya: HOGE.Ahya = new HOGE.Ahya('unko');
+        return ahya.say();
     }
 
 });
