da9d5e9ba2bd940bae98002610c571cc4f9b301f
funnythingz <hiroki@hackleberry.me>
2013-11-16 14:38:48 +0900

    added iron-router

--- client/skel.ts
+++ client/skel.ts
@@ -1,10 +1,10 @@
 /// <reference path="../packages/typescript-libs/node.d.ts" />
 /// <reference path="../packages/typescript-libs/meteor.d.ts" />
 
-if (Meteor.isClient) {
+Template['hello'].helpers({
 
-  Template['hello']['greeting'] = function () {
-    return "Typescript-compile of Meteor";
-  }
+    greeting: function() {
+        return "Typescript-compile of Meteor";
+    }
 
-}
+});
